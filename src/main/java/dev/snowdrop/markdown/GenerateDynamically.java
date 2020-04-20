d4a69bf9339b16f0795870b0ce16714813702fc7
Charles Moulliard <cmoulliard@redhat.com>
2020-04-20 19:14:55 +0200

    Fix code to add listitem

--- src/main/java/dev/snowdrop/markdown/GenerateDynamically.java
+++ src/main/java/dev/snowdrop/markdown/GenerateDynamically.java
@@ -10,12 +10,21 @@ public class GenerateDynamically {
         Document doc = new Document();
 
         Paragraph p = new Paragraph();
-
         Link link = new Link();
         link.setDestination("/exampleUrl");
         p.appendChild(link);
-
         doc.appendChild(p);
+
+        BulletList bl = new BulletList();
+        ListItem li = new ListItem();
+        li.appendChild(new Text("Task 1"));
+        bl.appendChild(li);
+
+        li = new ListItem();
+        li.appendChild(new Text("Task 2"));
+        bl.appendChild(li);
+
+        doc.appendChild(bl);
 
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         System.out.println(renderer.render(doc));  // "<p><a href="/exampleUrl"></a></p>"
